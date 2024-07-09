790779b3f8bdd1a42bbedd79e593534e82715e6c
Gustavo Rizon Wolschick <134110747+GustavoWolschick@users.noreply.github.com>
2024-07-09 19:41:47 -0300

    Inicio do back end do perfil
    Comit para alinhar o projeto.

--- Classes/Usuario.php
+++ Classes/Usuario.php
@@ -44,6 +44,49 @@ class Usuario
         return false;
     }
 
+    public function ler($search = '', $order_by = '')
+    {
+        $query = "SELECT * FROM " . $this->table_name;
+        $conditions = [];
+        $params = [];
+
+        if ($search) {
+            $conditions[] = " (nome LIKE :search OR email LIKE :search)";
+            $params[':search'] = '%' . $search . '%';
+        }
+
+        if ($order_by === 'nome') {
+            $query .= " ORDER BY nome";
+        } elseif ($order_by === 'sexo') {
+            $query .= " ORDER BY sexo";
+        }
+
+        if (count($conditions) > 0) {
+            $query .= " WHERE " . implode(' AND ', $conditions);
+        }
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute($params);
+        return $stmt;
+    }
+
+    public function lerPorId($id)
+    {
+        $query = "SELECT * FROM " . $this->table_name . " WHERE id = ?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute([$id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+
+    public function atualizar($id, $nome, $email, $telefone, $sexo, $senha, $foto, $nascimento, $adm, $ativo)
+    {
+        $query = "UPDATE " . $this->table_name . " SET nome = ?, email = ?, telefone = ?, sexo = ?, senha = ?, foto = ?, nascimento = ?, adm = ?, ativo = ? WHERE id = ?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute([$id, $nome, $email, $telefone, $sexo, $senha, $foto, $nascimento, $adm, $ativo]);
+        return $stmt;
+    }
+
 
 
 
